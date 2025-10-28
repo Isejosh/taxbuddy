0e9949fa8a6701699f9fcf9e1c319ae981004fa6
Isejosh <joshuayo52@gmail.com>
2025-10-28 01:21:02 +0100

    index done, updated style and javascript

---
+++ js/style.js
@@ -0,0 +1,22 @@
+// onboarding.js
+const slides = document.querySelectorAll('.onboarding_slide');
+const dots = document.querySelectorAll('.dot');
+const buttons = document.querySelectorAll('.onboarding_btn');
+let current = 0;
+
+buttons.forEach((btn, index) => {
+  btn.addEventListener('click', () => {
+    slides[current].classList.remove('active');
+    dots[current].classList.remove('active');
+
+    // If it's the last slide, go to login
+    if (current === slides.length - 1) {
+      window.location.href = "login.html";
+      return;
+    }
+
+    current++;
+    slides[current].classList.add('active');
+    dots[current].classList.add('active');
+  });
+});
